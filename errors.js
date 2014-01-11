272137b2601f8adabdc14d72be8f8f6a18b6eb80
nifty <niftykins@gmail.com>
2014-01-12 06:34:24 +1100

    bootstrap3, optional class

--- errors.js
+++ errors.js
@@ -1,10 +1,14 @@
 Errors = {
 	collection: new Meteor.Collection(null),
 
-	throw: function(message) {
-		Errors.collection.insert({ message: message, seen:false });
+	throw: function(message, type) {
+		type = type || "danger";
+		Errors.collection.insert({ message: message, type: type, seen: false });
 	},
 	clearSeen: function() {
 		Errors.collection.remove({seen: true});
+	},
+	cleanAll: function() {
+		Errors.collection.remove({});
 	}
 };
